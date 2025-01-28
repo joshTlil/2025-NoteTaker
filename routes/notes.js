b6f84e3da7754d3f5b25ef528e34f2d3eb4f5360
JoshuaT <jttsat170@gmail.com>
2025-01-28 13:16:58 -0500

    Working with routes seeing how they work and how to implement

---
+++ routes/notes.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+
+router.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 app.use(express.static("public"))
+
+// const noteRouter = require('./routes/notes')
+
+// app.use('/notes', noteRouter)
+
+app.get('/notes', (req, res)=>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
 app.listen(3000)
